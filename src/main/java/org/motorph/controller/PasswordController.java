479c9af1de8c0311096edc316133b8da222037f2
djkier <djjustinekier@gmail.com>
2025-04-27 22:46:35 +0800

    Update: Fix password interface, Add: color on the password interface

--- src/main/java/org/motorph/controller/PasswordController.java
+++ src/main/java/org/motorph/controller/PasswordController.java
@@ -7,13 +7,44 @@ import javax.swing.*;
 
 public class PasswordController {
     private PasswordInterface passwordInterface;
+    private JFrame app;
+    private JButton submitButton;
+    private String userName;
+    private String password;
 
     public PasswordController() {
+        this.userName = "admin";
+        this.password = "password";
         this.passwordInterface = new PasswordInterface("Login");
+        this.app = passwordInterface.getApp();
+        this.submitButton = passwordInterface.getSubmitButton();
+
+
+        this.submitButton.addActionListener(e ->
+                checkPassword(passwordInterface.getUserNameText()
+                        , passwordInterface.getPasswordText()));
+
+
     }
 
     public void start() {
-        passwordInterface.getApp().setVisible(true);
+        app.setVisible(true);
+    }
+
+    public void checkPassword(String eUserName, String ePassword) {
+        if (userName.equals(eUserName) && password.equals(ePassword)) {
+            app.dispose();
+            SwingUtilities.invokeLater(() -> {
+                ViewController vc = new ViewController();
+                vc.startApplication();
+            });
+        } else if (userName.equals(eUserName)){
+            System.out.println("Wrong password!");
+        } else {
+            System.out.println("No Username found!");
+        }
+        passwordInterface.restartField();
+
     }
 
 
--- src/main/java/org/motorph/view/PasswordInterface.java
+++ src/main/java/org/motorph/view/PasswordInterface.java
@@ -6,14 +6,16 @@ import java.awt.*;
 public class PasswordInterface {
     private JFrame passwordInterface;
     private JTextField userName;
-    private JTextField password;
+    private JPasswordField password;
     private JButton submitButton;
 
     public PasswordInterface(String title) {
         this.userName = new JTextField(16);
-        this.password = new JTextField(16);
+        this.password = new JPasswordField(16);
         this.submitButton = new JButton("Submit");
         this.passwordInterface = setUpFrame(title);
+        this.passwordInterface.getRootPane().setDefaultButton(submitButton);
+
     }
 
     private JFrame setUpFrame(String title) {
@@ -22,28 +24,48 @@ public class PasswordInterface {
         passUI.setLocation(1250,120);
         passUI.setSize(350, 220);
         passUI.setLayout(new FlowLayout());
-        passUI.add(entry());
+        passUI.setResizable(false);
+        passUI.getContentPane().setBackground(Color.PINK);
 
+        passUI.add(entryField(new JLabel()));
         return passUI;
     }
 
-    public JPanel entry() {
+    public JPanel entryField(JLabel label) {
+        label.setForeground(Color.RED);
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(6,1));
         panel.add(new JPanel());
         panel.add(newRow("Username", userName));
         panel.add(newRow("Password", password));
-        panel.add(new JPanel());
+        panel.add(label);
         panel.add(submitButton);
+        panel.setBackground(Color.PINK);
 
         return panel;
     }
 
+    public void restartField() {
+        userName.setText("");
+        password.setText("");
+        passwordInterface.getContentPane().removeAll();
+        passwordInterface.add(entryField(new JLabel("Invalid username or password.")));
+        passwordInterface.revalidate();
+        passwordInterface.repaint();
+        passwordInterface.getRootPane().setDefaultButton(submitButton);
+
+    }
+
+
     private JPanel newRow(String str, JTextField e) {
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
-        panel.setSize(100, 50);
-        panel.add(new JLabel(str), BorderLayout.WEST);
+
+        JLabel input = new JLabel(str);
+
+        panel.add(input, BorderLayout.WEST);
         panel.add(e, BorderLayout.EAST);
         return panel;
     }
@@ -51,6 +73,21 @@ public class PasswordInterface {
     public JFrame getApp() {
         return this.passwordInterface;
     }
+
+    public JButton getSubmitButton() {
+        return this.submitButton;
+    }
+
+    public String getUserNameText() {
+        return this.userName.getText();
+    }
+
+    public String getPasswordText() {
+        char[] pw = password.getPassword();
+        System.out.println(pw);
+        return new String(pw);
+    }
+
 
 
 
