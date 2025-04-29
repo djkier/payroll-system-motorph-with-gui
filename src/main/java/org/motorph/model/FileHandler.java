20f68006e525a10d94bb5e5d8b6649a0064f7c8b
djkier <djjustinekier@gmail.com>
2025-04-29 23:02:36 +0800

    Add: File validator

--- src/main/java/org/motorph/model/FileHandler.java
+++ src/main/java/org/motorph/model/FileHandler.java
@@ -1,8 +1,6 @@
 package org.motorph.model;
 
 import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class FileHandler {
@@ -12,24 +10,26 @@ public class FileHandler {
         this.lines = new ArrayList<>();
     }
 
+
     public void printFile() {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < this.lines.size(); i++) {
             System.out.println(lines.get(i));
         }
     }
 
     public void testing() {
+        long startTime = System.currentTimeMillis();
         this.lines = preLoadEmpDetails();
-
+        System.out.println(isValidEmployeeDetailFile(this.lines));
         printFile();
 
         this.lines = preLoadAttendance();
+        System.out.println(isValidEmployeeDetailFile(this.lines));
         printFile();
 
+        long endTime = System.currentTimeMillis();
+        System.out.println("Process took " + (endTime - startTime) + "ms");
     }
-
-
-
 
     public ArrayList<String> openFile(String filePath) throws FileNotFoundException {
         InputStream is = new FileInputStream(filePath);
@@ -69,6 +69,46 @@ public class FileHandler {
         }
 
         return list;
+    }
+
+
+    public Boolean isValidFile(ArrayList<String> file, String comparedTo) {
+        String[] w = comparedTo.split(",");
+        String[] fHead = file.get(0).split(",");
+
+        //Check coloumn title
+        if (w.length != fHead.length) {
+            return false;
+        }
+
+        for (int i = 0; i < w.length; i++) {
+            if (!w[i].trim().equalsIgnoreCase(fHead[i].trim())) {
+                return false;
+            }
+        }
+
+        //Check entry length per row
+        for (int i = 1; i < file.size(); i++) {
+            String[] row = cleanSplit(file.get(i));
+            if (w.length != row.length){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public Boolean isValidEmployeeDetailFile(ArrayList<String> file) {
+        return isValidFile(file, FileHeader.detailHeader);
+    }
+
+    public Boolean isValidAttendanceFile(ArrayList<String> file) {
+        return isValidFile(file, FileHeader.attendanceHeader);
+    }
+
+    public String[] cleanSplit(String line) {
+        return line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
     }
 
 }
---
+++ src/main/java/org/motorph/model/FileHeader.java
@@ -0,0 +1,7 @@
+package org.motorph.model;
+
+public class FileHeader {
+
+    public static String detailHeader = "Employee #,Last Name,First Name,Birthday,Address,Phone Number,SSS #,Philhealth #,TIN #,Pag-ibig #,Status,Position,Immediate Supervisor,Basic Salary,Rice Subsidy,Phone Allowance,Clothing Allowance,Gross Semi-monthly Rate,Hourly Rate";
+    public static String attendanceHeader = "Employee #,Last Name,First Name,Date,Log In,Log Out";
+}
