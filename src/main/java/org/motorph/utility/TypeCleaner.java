be68abbf2a6262aab06792460882d18582783e15
djkier <djjustinekier@gmail.com>
2025-04-30 16:19:26 +0800

    Add: new static method to be use by the attendance and for future methods

--- src/main/java/org/motorph/utility/TypeCleaner.java
+++ src/main/java/org/motorph/utility/TypeCleaner.java
@@ -1,5 +1,9 @@
 package org.motorph.utility;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class TypeCleaner {
 
     public static String[] cleanSplit(String line) {
@@ -11,6 +15,21 @@ public class TypeCleaner {
         String removeComma = money.replace(",", "");
         String removeQuotation = removeComma.replace("\"", "");
         return Double.valueOf(removeQuotation);
+    }
+
+    public static LocalDate convertStringToDate(String date) {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        return LocalDate.parse(date, format);
+    }
+
+    public static LocalTime convertStringToTime(String time) {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm");
+        return LocalTime.parse(time, format);
+    }
+
+    //Get only 2 decimal numbers
+    public static double cleanDouble(double number) {
+        return Math.round(number * 100.0) / 100.0;
     }
 
 }
