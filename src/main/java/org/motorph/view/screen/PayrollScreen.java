2d0d055dffa84eee1347746b0e94a8386aae91ee
djkier <djjustinekier@gmail.com>
2025-05-05 20:32:46 +0800

    Update: UI on payslip, realign the options and its components

--- src/main/java/org/motorph/view/screen/PayrollScreen.java
+++ src/main/java/org/motorph/view/screen/PayrollScreen.java
@@ -2,8 +2,10 @@ package org.motorph.view.screen;
 
 import org.motorph.utility.styling.ColorUtility;
 import org.motorph.utility.styling.EffectsUtility;
+import org.motorph.utility.styling.FontUtility;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -29,9 +31,14 @@ public class PayrollScreen extends ScreenView {
         b.setLayout(null);
 
         JPanel selection = selection();
-        selection.setBounds(0,0, 600, 400);
+        selection.setBounds(0,0, 800, 230);
 
+        JPanel horizontalDivider = EffectsUtility.horizontalDivider(24, ColorUtility.grayLight);
+        horizontalDivider.setBounds(0, 130,800, 24);
+
+//        b.add(horizontalDivider);
         b.add(selection);
+
 
         return b;
     }
@@ -43,12 +50,21 @@ public class PayrollScreen extends ScreenView {
 
     private JPanel selection(){
         JPanel panel = new JPanel();
+        panel.setBackground(ColorUtility.white);
         panel.setLayout(null);
 
-        JRadioButton fullMonthRadio = new JRadioButton("Full / Half Month");
+
+        JLabel title = new JLabel("Choose Date Range for Payslip / Report");
+        title.setFont(FontUtility.important(16));
+
+        JRadioButton fullMonthRadio = new JRadioButton("Full/Half Month");
+        fullMonthRadio.setBackground(ColorUtility.white);
+        fullMonthRadio.setFont(FontUtility.important(14));
         fullMonthRadio.setFocusPainted(false);
 
         JRadioButton customRangeRadio = new JRadioButton("Custom Range");
+        customRangeRadio.setBackground(ColorUtility.white);
+        customRangeRadio.setFont(FontUtility.important(14));
         customRangeRadio.setFocusPainted(false);
 
         ButtonGroup group = new ButtonGroup();
@@ -56,16 +72,20 @@ public class PayrollScreen extends ScreenView {
         group.add(customRangeRadio);
 
         JPanel cardPanel = new JPanel();
+        cardPanel.setBackground(ColorUtility.white);
         cardPanel.setLayout(null);
 
         JPanel fullMonthPanel = fullMonthPanel();
-        fullMonthPanel.setBounds(0, 0, 400, 50);
+        fullMonthPanel.setBackground(ColorUtility.white);
+        fullMonthPanel.setBounds(0, 6, 700, 40);
 
         JPanel customRangePanel = customRangePanel();
-        customRangePanel.setBounds(0, 70, 400, 50);
+        customRangePanel.setBackground(ColorUtility.white);
+        customRangePanel.setBounds(13, 56, 700, 80);
 
         //default full month selection
         fullMonthRadio.setSelected(true);
+        fullMonthRadio.setBackground(ColorUtility.white);
         optionsComponent(fullMonthPanel, customRangePanel);
 
 
@@ -81,10 +101,13 @@ public class PayrollScreen extends ScreenView {
             optionsComponent(customRangePanel, fullMonthPanel);
         });
 
-        fullMonthRadio.setBounds(0,0,150,50);
-        customRangeRadio.setBounds(0, 60, 150, 50);
-        cardPanel.setBounds(150, 0, 500, 200);
+        title.setBounds(0, 0, 400, 20);
 
+        fullMonthRadio.setBounds(50,30,150,50);
+        customRangeRadio.setBounds(50, 70, 150, 50);
+        cardPanel.setBounds(210, 30, 800, 200);
+
+        panel.add(title);
         panel.add(fullMonthRadio);
         panel.add(customRangeRadio);
         panel.add(cardPanel);
@@ -95,37 +118,107 @@ public class PayrollScreen extends ScreenView {
 
     private JPanel fullMonthPanel() {
         JPanel fullMonthPanel = new JPanel();
+        fullMonthPanel.setLayout(null);
 
-
-        fullMonthPanel.add(new JLabel("Cutoff:"));
+        //CUT OFF
         String[] type = {"Full Month", "1st Half", "2nd Half"};
-        JComboBox<String> typeOfPayment = new JComboBox<>(type);
-        fullMonthPanel.add(typeOfPayment);
+        JPanel typeOfPayment = labelAndComboBox("Cut Off", type);
+        typeOfPayment.setBounds(0,0, 170,70);
 
-        fullMonthPanel.add(new JLabel("Month:"));
+
+        //MONTH OPTIONS
         String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-        JComboBox<String> monthSelect = new JComboBox<>(months);
-        fullMonthPanel.add(monthSelect);
+        JPanel chooseMonth = labelAndComboBox("Month", months);
+        chooseMonth.setBounds(175, 0, 170,70);
 
+
+        //YEAR OPTIONS
+        String[] years = {"2024", "2025", "2026"};
+        JPanel chooseYear = labelAndComboBox("Year", years);
+        chooseYear.setBounds(355, 0, 130,70);
+
+
+        fullMonthPanel.add(typeOfPayment);
+        fullMonthPanel.add(chooseMonth);
+        fullMonthPanel.add(chooseYear);
 
         return fullMonthPanel;
+    }
+
+    private JPanel labelAndComboBox(String title, String[] options) {
+        JPanel panel = new JPanel();
+        panel.setBackground(ColorUtility.white);
+
+        JLabel label = new JLabel(title);
+        label.setPreferredSize(new Dimension(50, 20));
+        if (title.equalsIgnoreCase("Year")) {
+            label.setPreferredSize(new Dimension(40, 20));
+        }
+        label.setFont(FontUtility.important(12));
+        label.setHorizontalAlignment(SwingConstants.LEFT);
+
+        JComboBox<String> list = new JComboBox<>(options);
+        list.setFont(FontUtility.plain(12));
+        list.setBackground(ColorUtility.white);
+        list.setBorder(new LineBorder(ColorUtility.grayBorder));
+
+        panel.add(label);
+        panel.add(list);
+
+        return panel;
     }
 
 
     private JPanel customRangePanel() {
         JPanel customRangePanel = new JPanel();
+        customRangePanel.setLayout(null);
 
-        customRangePanel.add(new JLabel("Start Date:"));
-        customRangePanel.add(new JTextField(10));
-        customRangePanel.add(new JLabel("End Date:"));
-        customRangePanel.add(new JTextField(10));
+        JPanel startDate = dateAndTextfield("Start Date");
+        startDate.setBounds(0,0,240,80);
 
+        JPanel endDate = dateAndTextfield("End Date");
+        endDate.setBounds(242,0,240,80);
+
+        customRangePanel.add(startDate);
+        customRangePanel.add(endDate);
 
         return customRangePanel;
     }
 
+    private JPanel dateAndTextfield(String title) {
+        JPanel panel = new JPanel();
+        panel.setBackground(ColorUtility.white);
+        panel.setLayout(null);
+
+        JLabel label = new JLabel(title);
+        JTextField textField = new JTextField(10);
+        JLabel hint = new JLabel("mm-dd-yyyy");
+        hint.setFont(FontUtility.plainItalic(11));
+//        hint.setFont(FontUtility.important(10));
+        hint.setForeground(ColorUtility.grayDark);
+
+
+        label.setBounds(0,0,70,20);
+        textField.setBounds(70,0,150,20);
+        hint.setBounds(80,16,150,20);
+
+        panel.add(label);
+        panel.add(textField);
+        panel.add(hint);
+
+        return panel;
+
+    }
+
     private void setEnabledComponent(JPanel panel, boolean bool) {
         for (Component c : panel.getComponents()) {
+            //Target components inside a component
+            if (c instanceof JComponent insideComp) {
+                for (Component i : insideComp.getComponents()) {
+                    i.setEnabled(bool);
+                }
+            }
+
             c.setEnabled(bool);
         }
     }
